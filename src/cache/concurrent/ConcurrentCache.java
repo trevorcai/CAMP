6c71daa438672551cc2c6d5136a9275b217d0c90
Trevor Cai <tycai29@gmail.com>
2016-04-24 19:51:37 -0700

    Striped read buffers

--- src/cache/concurrent/ConcurrentCache.java
+++ src/cache/concurrent/ConcurrentCache.java
@@ -7,6 +7,7 @@ import cache.admission.IdlePolicy;
 
 import java.util.Map;
 import java.util.Queue;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
@@ -20,11 +21,10 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /* Design inspiration from ConcurrentLinkedHashMap */
 public abstract class ConcurrentCache implements Cache {
-    /** Buffer thresholds. */
+    /** Buffer thresholds and size. */
     private static final int READ_THRESHOLD = 32;
     private static final int READ_MAX_DRAIN = 2 * READ_THRESHOLD;
     private static final int READ_BUFFER_SIZE = 4 * READ_MAX_DRAIN;
-
     private static final int READ_MASK = READ_BUFFER_SIZE - 1;
 
     /** The maximum number of write operations to perform per amortized drain. */
@@ -40,10 +40,11 @@ public abstract class ConcurrentCache implements Cache {
     final int capacity;
     final AtomicInteger load = new AtomicInteger(0);
 
-    /** Buffer and its counter */
-    private final AtomicReference<MapNode>[] buffer;
-    private long bufferReadPointer = 0;
-    private final AtomicLong bufferWritePointer = new AtomicLong();
+    /** Buffers and their counters */
+    private final int numBuffers;
+    private final AtomicReference<MapNode>[][] buffers;
+    private final long[] bufferReadPointer;
+    private final AtomicLong[] bufferWritePointer;
     private final Queue<MapNode> writeBuffer;
 
     /** Tracks the status of the drain */
@@ -55,6 +56,9 @@ public abstract class ConcurrentCache implements Cache {
     /** Admission Policy */
     private final AdmissionPolicy policy;
 
+    /** Random number generator for which buffer to drain */
+    private final Random generator = new Random();
+
     @SuppressWarnings("unchecked")
     public ConcurrentCache(int capacity, int concurrency,
                            AdmissionPolicy policy) {
@@ -62,15 +66,27 @@ public abstract class ConcurrentCache implements Cache {
         this.policy = policy;
         data = new ConcurrentHashMap<>(128, 0.75f, concurrency);
         writeBuffer = new ConcurrentLinkedQueue<>();
-        buffer = new AtomicReference[READ_BUFFER_SIZE];
-        for (int i = 0; i < READ_BUFFER_SIZE; i++) {
-            buffer[i] = new AtomicReference<>();
+        numBuffers = ceilingNextPowerOfTwo(concurrency);
+        buffers = new AtomicReference[numBuffers][READ_BUFFER_SIZE];
+        bufferReadPointer = new long[numBuffers];
+        bufferWritePointer = new AtomicLong[numBuffers];
+        for (int i = 0; i < numBuffers; i++) {
+            bufferReadPointer[i] = 0;
+            bufferWritePointer[i] = new AtomicLong();
+            for (int j = 0; j < READ_BUFFER_SIZE; j++) {
+                buffers[i][j] = new AtomicReference<>();
+            }
         }
         pool = Executors.newCachedThreadPool();
     }
 
     public ConcurrentCache(int capacity, int concurrency) {
         this(capacity, concurrency, IdlePolicy.getInstance());
+    }
+
+    private static int ceilingNextPowerOfTwo(int x) {
+        // From CLHM source code
+        return 1 << (Integer.SIZE - Integer.numberOfLeadingZeros(x - 1));
     }
 
     @Override
@@ -80,12 +96,13 @@ public abstract class ConcurrentCache implements Cache {
             return null;
         }
 
-        long writePtr = bufferWritePointer.get();
+        int bufIndex = getBufferIndex();
+        long writePtr = bufferWritePointer[bufIndex].get();
         int index = (int) writePtr & READ_MASK;
-        if (buffer[index].compareAndSet(null, result)) {
-            bufferWritePointer.incrementAndGet();
+        if (buffers[bufIndex][index].compareAndSet(null, result)) {
+            bufferWritePointer[bufIndex].incrementAndGet();
         }
-        asyncDrainIfNeeded();
+        asyncDrainIfNeeded(bufIndex);
         return result.getValue();
     }
 
@@ -113,26 +130,37 @@ public abstract class ConcurrentCache implements Cache {
 
     abstract void doRead(MapNode node);
     abstract void doWrite(MapNode node);
-
     abstract void evict();
 
+    private int getBufferIndex() {
+        return (int) Thread.currentThread().getId() & (numBuffers - 1);
+    }
+
     private void asyncDrainIfNeeded() {
-        if (shouldDrain()) {
+        asyncDrainIfNeeded(-1);
+    }
+
+    private void asyncDrainIfNeeded(int idx) {
+        if (shouldDrain(idx)) {
             pool.execute(this::tryDrain);
         }
     }
 
     /** Checks if buffer should be drained */
-    private boolean shouldDrain() {
+    private boolean shouldDrain(int idx) {
         if (drainActive) {
             return false;
         } else if (isEager.get()) {
             return true;
         }
 
-        long toConsume = bufferWritePointer.get() - bufferReadPointer;
-
-        return toConsume > READ_THRESHOLD;
+        if (idx > 0) {
+            long toConsume =
+                    bufferWritePointer[idx].get() - bufferReadPointer[idx];
+            return toConsume > READ_THRESHOLD;
+        } else {
+            return false;
+        }
     }
 
     /** Tries to drain buffer, if someone else isn't already draining it */
@@ -146,26 +174,34 @@ public abstract class ConcurrentCache implements Cache {
         lock.unlock();
     }
 
-    void drain() {
-        drainReadBuffer();
+    private void drain() {
+        drainReadBuffers();
         drainWriteBuffer();
     }
 
-    private void drainReadBuffer() {
-        int undrained = (int) (bufferWritePointer.get() - bufferReadPointer);
+    private void drainReadBuffers() {
+        int idx = generator.nextInt(numBuffers);
+        for (int i = 0; i < numBuffers; i++) {
+            drainReadBuffer((idx + i) & (numBuffers - 1));
+        }
+    }
+
+    private void drainReadBuffer(int idx) {
+        int undrained =
+                (int) (bufferWritePointer[idx].get() - bufferReadPointer[idx]);
         // Drain at most READ_MAX_DRAIN elements
         int toDrain = (undrained < READ_MAX_DRAIN) ? undrained : READ_MAX_DRAIN;
         for (int i = 0; i < toDrain; i++) {
-            int index = (int) bufferReadPointer & READ_MASK;
-            MapNode n = buffer[index].get();
+            int index = (int) bufferReadPointer[idx] & READ_MASK;
+            MapNode n = buffers[idx][index].get();
             // Shouldn't happen
             if (n == null) {
                 break;
             }
 
-            buffer[index].lazySet(null);
+            buffers[idx][index].lazySet(null);
             doRead(n);
-            bufferReadPointer++;
+            bufferReadPointer[idx]++;
         }
     }
 
